28435bf098eea566a5794980194c177f77bd3eff
M'Bemba Fofana <mbemba.fofana@gmail.com>
2019-06-22 15:23:24 +0200

    project init

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html class="h-100" lang="fr">
+<head>
+	<meta charset="utf-8">
+	<meta HTTP-EQUIV="pragma" content="no-cache">
+	<meta name="viewport" content="width-device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/jquery3.4.0.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- <link rel="stylesheet" href="css/bootstrap3.min.css"> -->
+	<title>Index</title>
+</head>
+<body class="d-flex flex-column h-100">
+	<header>
+		<?php require_once("require_once/menu.php") ?>
+	</header>
+		<div class="container">
+			<div class="row">
+				<div id="content" class="d-flex flex-column col-md-6">
+					<h1>Bienvenue sur mon Portfolio</h1>
+
+					<br/><br/>
+
+					<p>Je me présente, M’Bemba Fofana, actuellement en reconversion professionnelle, je prépare BTS SIO (Services Informatiques aux Organisations) option SLAM (Solutions Logicielles et applications métiers) au sein du groupe Gefor en 1 an.</p>
+					<p>Pour des informations supplémentaires sur ma personne, vous pouvez consulter mon CV en <a href="curriculum_vitae.php">cliquant ici.</a></p>
+					<p>Vous trouverez sur ce portfolio de nombreuse informations sur ma formation , les travaux menés , mes expériences professionnelles.</p>
+					<br/><br/>
+					<h2>Filière</h2>
+					<p>Le BTS SIO ( Services Informatiques aux Organisations à été créé en 2011, anciennement connu sous le nom de BTS IG ( Informatique de Gestion ).</p>
+					<p>Ce BTS nous offre 2 options :</p>
+					<p>    • SISR « solutions d’infrastructure, systèmes et réseaux »</p>
+					<p>    • SLAM « solutions logicielles et applications métiers »</p>
+
+					<p>Pour ma part , j’ai choisi l’option SLAM qui est plus porté solutions logicielles et applicatives ( développement de logiciels personnalisés, de site web ).</p>
+
+					<p>Option SLAM:</p>
+					<p>L’élève aura pour but de Participer à la production et à la fourniture de services informatiques aux organisations, soit en tant que collaborateur de l’organisation soit en tant qu’intervenant d’une société de conseil et de services informatiques, d’un éditeur de logiciels ou d’une société de conseil en technologies.</p>
+					<p>Dans le cadre de l’option SLAM (Solutions Logicielles et Applications Métiers), le titulaire du diplôme intervient plus particulièrement dans :</p>
+					      
+					<p>    • la définition des spécifications techniques à partir de l’expression des besoins des utilisateurs et des contraintes de l’organisation préalablement recensés.</p>
+					<p>    • la réalisation ou l’adaptation puis la validation de solutions applicatives à l’aide des environnements de développement retenus.
+					la gestion du patrimoine applicatif ;</p>
+					<p>    • la rédaction de la documentation d’une solution applicative et la formation des utilisateurs.</p>
+					<p>    • la recherche de réponses adaptées à des problèmes liés à la fourniture des services informatiques.</p>
+					<p>    • l’accompagnement et l’assistance des utilisateurs, le maintien de la qualité des services informatiques.</p>
+				</div>
+			</div> <!--Fin div class="row"-->
+		</div> <!--Fin div class="container"-->
+	<footer></footer>
+
+</body>
+</html>
